2a5b4631e83fe064f29eda5bebca651e08a36a6f
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-02-19 20:21:45 -0800

    Add autolayout constraints

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 0
+        
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    @IBAction func shakeButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        ballImageView.image = UIImage(named: ballArray[randomBallNumber])
     }
 
 
